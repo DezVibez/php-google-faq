2099bc0f83e27e44b3912ff7bb8773ff959b7dce
Emanuele <103491766+DezVibez@users.noreply.github.com>
2022-08-31 14:32:49 +0200

    prima sezione completa render ok

--- index.php
+++ index.php
@@ -3,13 +3,34 @@
 
 $sezioni = [
     [
-        'domanda' => ' <h1> come si fa questo <h1> ',
-        'risposte' => ['si fa cosi', 'si fa cosà']
+        'domanda' => "<h2> Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio? <h2>",
+        'risposte' => ["La recente decisione della Corte di giustizia dell'Unione europea ha profonde conseguenze per i motori di ricerca in Europa.
+                         La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, 
+                         irrilevanti o non più rilevanti, o eccessivi.",
+                        "Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta 
+                        e effettuare un bilanciamento tra il diritto dell'individuo
+                         a controllare i suoi dati personali ed il diritto di tutti 
+                         di conoscere e distribuire le informazioni.",
+                         "Per presentare una richiesta di rimozione, compila questo modulo web. Riceverai una risposta automatica che conferma la ricezione della tua richiesta. Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po' di tempo perché abbiamo già ricevuto tante richieste simili). Nel valutare la richiesta, considereremo se i risultati includono informazioni obsolete relative alla tua vita privata. Considereremo inoltre se vi è un interesse pubblico a che le informazioni rimangano nei nostri risultati delle ricerche, 
+                         ad esempio se riguardano frodi finanziarie, negligenza professionale, condanne penali o la tua condotta pubblica in relazione a un pubblico ufficio (eletto o non eletto). Queste valutazioni sono complesse e,
+                          in quanto organizzazione privata, potremmo non essere nella posizione giusta per prendere decisioni in merito al tuo caso. Se non sei d'accordo con la nostra valutazione, puoi rivolgerti all'Autorità garante per la protezione dei dati personali nel tuo paese.",
+                          "Nei prossimi mesi lavoreremo a stretto contatto con le autorità per la protezione dei dati e con altre autorità per perfezionare il nostro approccio. La decisione della CGUE rappresenta un cambiamento significativo per i motori di ricerca. Siamo preoccupati per le sue conseguenze, ma riteniamo anche che sia importante rispettare la decisione della Corte e ci stiamo adoperando per predisporre una procedura conforme alla legge.",
+                          "Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato che i risultati potrebbero essere stati modificati nel rispetto delle leggi europee per la protezione dei dati. Mostriamo questa notifica agli utenti europei quando cercano la maggior parte dei nomi, non soltanto le pagine che sono state soggette a una rimozione."]
     ],
     [
-        'domanda' => ' <h1> come si fa quello <h1>',
+        'domanda' => '<h2> Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro? <h2>',
+        'risposte' => ["Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un'elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.", 
+        "si fa come qua",
+        ""]
+    ],
+    [
+        'domanda' => '<h2> Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro? <h2>',
         'risposte' => ['si fa come la', 'si fa come qua']
     ],
+    [
+        'domanda' => '<h2> Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro? <h2>',
+        'risposte' => ['si fa come la', 'si fa come qua']
+    ]
     ];
 
 
@@ -22,18 +43,20 @@ $sezioni = [
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Google Faqs</title>
 </head>
 <body>
     <ul>
         <?php foreach($sezioni as $sezione){?>
             <li> 
-                <div> <?php echo $sezione['domanda'] ?> </div>  
-                <ol>
-                    <?php foreach($sezione['risposte'] as $risposte) {?>
-                        <li> <?php echo $risposte ?></li>
-                    <?php }?>
-                </ol>
+                <div> 
+                    <?php echo $sezione['domanda'] ?>
+                </div>  
+                
+                <?php foreach($sezione['risposte'] as $risposte) {?>
+                    <div class="risposte"> <?php echo $risposte ?></div>
+                <?php }?>
+                
             </li>
             
         <?php }?>
@@ -46,6 +69,10 @@ $sezioni = [
 
     * {
         list-style-type: none;
+    }
+
+    .risposte{
+        margin: 30px 0;
     }
 
 </style>
